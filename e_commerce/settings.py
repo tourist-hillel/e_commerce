822b93c62be43e3d8712667915f765e1e1246d52
Inferato <infaustmager@gmail.com>
2026-05-21 19:37:38 +0300

    feat[Hillel-1]: New auth class added

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -204,7 +204,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'shop.api.authetication.EcommerceJWTAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         # 'rest_framework.authentication.TokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
